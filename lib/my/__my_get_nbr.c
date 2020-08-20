d44f055ec63c288d85713e75c3e428cd019c7c86
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-08-20 05:33:03 +0200

    Added my_convert_base() (Closes #10)

--- lib/my/__my_get_nbr.c
+++ lib/my/__my_get_nbr.c
@@ -7,40 +7,39 @@
 
 #include "my.h"
 
-static int __my_get_nbr_get_sign(char **tmp)
+static int __my_get_nbr_get_sign(char **tmp, const size_t _m)
 {
     int sign_multiplier = 1;
 
-    for (; **tmp; *tmp += 1) {
+    for (; **tmp && (**tmp == '-' || **tmp == '+'); *tmp += 1) {
         if (**tmp == '-') {
             sign_multiplier *= -1;
-        } else if (**tmp == '+') {
-            continue;
-        } else {
+            if (_m & ONE_NEG_SIGN)
+                break;
+        } else if (_m & ONE_NEG_SIGN) {
             break;
         }
     }
     return (sign_multiplier);
 }
 
-size_t __my_get_nbr(const char *__str, const size_t _m)
+size_t __my_get_nbr(const char *__str, const char base[], const size_t _m)
 {
+    const ssize_t len_base = my_strlen(base);
+    ssize_t index = 0;
     char *tmp = (char *)__str;
     size_t nb = 0;
     int sign_multiplier = 1;
 
-    if (!__str)
+    if (!__str || len_base <= 1)
         return (0);
     if (_m & SIGNED)
-        sign_multiplier = __my_get_nbr_get_sign(&tmp);
+        sign_multiplier = __my_get_nbr_get_sign(&tmp, _m);
     for (; *tmp; tmp += 1)  {
-        if (!my_is_num(*tmp)) {
+        index = my_str_contains_index(base, *tmp);
+        if (index < 0)
             break;
-        }
-        if (nb == 0 && *tmp == '0') {
-            continue;
-        }
-        nb = nb * 10 + *tmp - '0';
+        nb = nb * len_base + index;
     }
     return (nb * sign_multiplier);
 }
